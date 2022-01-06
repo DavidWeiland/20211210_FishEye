44fa5a2af95768d7f6a06c0a46cb65a592b043aa
David Weiland <weiland.david@icloud.com>
2022-01-06 14:04:31 +0100

    card test

---
+++ frontend/src/Components/Card/index.test.jsx
@@ -0,0 +1,33 @@
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import Card from './index'
+
+let container = null
+
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
+
+describe('When component is called,', () => {
+  it("photographer's name isn't displayed if there are no props", () => {
+    act(() => {
+      render(<Card tags={[]} />, container)
+    })
+    expect(container.textContent).toBe(',  €/jour,')
+  
+  })
+  
+  it("photographer's name is displayed if there are props", () => {
+    act(() => {
+        render(<Card tags={["Portrait", "Art"]} name='David' />, container)
+    })
+    expect(container.textContent).toBe('David,  €/jour,#Portrait#Art')
+  })
+})
